a94653d9d3d16c940dbcfc3a09844fd42c5856d3
Thomas Levesque <thomaslevesque@users.noreply.github.com>
2015-09-03 00:50:07 +0200

    Fixed wrong dependency version in nuspec

--- NString/Properties/AssemblyInfo.cs
+++ NString/Properties/AssemblyInfo.cs
@@ -2,9 +2,6 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("NString")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -15,7 +12,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyVersion("1.1.6.0")]
-[assembly: AssemblyFileVersion("1.1.6.0")]
+[assembly: AssemblyVersion("1.1.7.0")]
+[assembly: AssemblyFileVersion("1.1.7.0")]
 
 [assembly: InternalsVisibleTo("NString.Tests")]
